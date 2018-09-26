fb16814d3987d3bae1073056a85d1c0fb8499756
Arthur Ragimov <ragimov.artyr@gmail.com>
2018-09-26 16:20:59 +0300

    Updated app/src/main/java/com/strikepro/catalog/fragment/BlogFragment: 	release logic with dummy data; Updated app/src/main/java/com/strikepro/catalog/model/Post: 	fix model properties; Updated app/src/main/res/layout/fragment_blog: 	replace "RecyclerView" -> "GridView"; 	update constraints;

--- app/src/main/java/com/strikepro/catalog/fragment/BlogFragment.kt
+++ app/src/main/java/com/strikepro/catalog/fragment/BlogFragment.kt
@@ -1,11 +1,17 @@
 package com.strikepro.catalog.fragment
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
 import com.strikepro.catalog.R
+import com.strikepro.catalog.model.Post
+import kotlinx.android.synthetic.main.blog_item.view.*
+import kotlinx.android.synthetic.main.fragment_blog.*
 
 class BlogFragment : Fragment() {
 
@@ -13,6 +19,45 @@ class BlogFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_blog, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        post_list.adapter = PostAdapter(activity as Context)
+    }
+
+    private class PostAdapter(private val mContext: Context) : BaseAdapter() {
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val postView: View = convertView ?: (mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater)
+                    .inflate(R.layout.blog_item, parent, false)
+
+            val post: Post = dummyData[position]
+
+            postView.post_logo.setImageResource(R.mipmap.ic_launcher) // TODO: replace to load from URLs
+            postView.post_title.text = post.title
+            postView.post_shortdesc.text = post.shortDesc
+            postView.post_timestamp.text = post.timestamp
+            postView.post_share.setOnClickListener { Log.d(TAG, "post_share button click") }
+
+            return postView
+        }
+
+        override fun getItem(position: Int): Any? = null
+
+        override fun getItemId(position: Int): Long = 0L
+
+        override fun getCount(): Int = dummyData.size
+
+        companion object {
+            const val TAG: String = "PostAdapter"
+
+            val dummyData: Array<Post> = arrayOf(
+                    Post("1 post title", "https://picsum.photos/g/400/200", "14.06.2015", "1 post short desc"),
+                    Post("2 post title", "https://picsum.photos/g/400/200", "15.07.2016", "2 post short desc"),
+                    Post("3 post title", "https://picsum.photos/g/400/200", "16.08.2017", "3 post short desc")
+            )
+        }
     }
 
     companion object {
--- app/src/main/java/com/strikepro/catalog/model/Post.kt
+++ app/src/main/java/com/strikepro/catalog/model/Post.kt
@@ -1,6 +1,8 @@
 package com.strikepro.catalog.model
 
-import java.net.URL
-import java.util.*
-
-class Post(var title: String, var logo: URL, var timestamp: Date)
+class Post(
+        var title: String,
+        var logo: String,
+        var timestamp: String,
+        var shortDesc: String
+)
