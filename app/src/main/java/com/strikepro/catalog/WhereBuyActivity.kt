904d08566a2fe71a19faf7fbd6f4ac5f205856e0
Arthur Ragimov <ragimov.artyr@gmail.com>
2018-10-13 12:30:16 +0300

    Updated app/src/main/java/com/strikepro/catalog/WhereBuyActivity: 	add cities spinner onItemSelected listener; Updated app/src/main/res/layout/activity_where_buy: 	fix margins for YandexMap view;

--- app/src/main/java/com/strikepro/catalog/WhereBuyActivity.kt
+++ app/src/main/java/com/strikepro/catalog/WhereBuyActivity.kt
@@ -2,13 +2,16 @@ package com.strikepro.catalog
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
 import com.yandex.mapkit.Animation
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.map.CameraPosition
 import kotlinx.android.synthetic.main.activity_where_buy.*
 
-class WhereBuyActivity : AppCompatActivity() {
+class WhereBuyActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,18 +26,36 @@ class WhereBuyActivity : AppCompatActivity() {
                 Animation(Animation.Type.SMOOTH, 0f),
                 null
         )
+
+        map_city.onItemSelectedListener = this
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        Log.d(TAG, "onNothingSelected")
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        Log.d(TAG, "onItemSelected; position=$position; id=$id")
     }
 
     override fun onStart() {
         super.onStart()
 
-        map_view.onStart()
-        MapKitFactory.getInstance().onStart()
+        startYandexMap()
     }
 
     override fun onStop() {
         super.onStop()
 
+        stopYandexMap()
+    }
+
+    private fun startYandexMap() {
+        map_view.onStart()
+        MapKitFactory.getInstance().onStart()
+    }
+
+    private fun stopYandexMap() {
         map_view.onStop()
         MapKitFactory.getInstance().onStop()
     }
